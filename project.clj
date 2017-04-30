39c2ec36b501639b4bb2097e641eb7f1073b605e
ronen <narkisr@gmail.com>
2017-05-01 01:08:42 +1000

    updating dependencies

--- project.clj
+++ project.clj
@@ -1,10 +1,11 @@
-(defproject formation "0.1.0"
+(defproject formation "0.1.1"
   :description "A configuration library for Clojure"
   :url "https://github.com/narkisr/fomration"
   :license  {:name "Apache License, Version 2.0" :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
   :dependencies [
      [org.clojure/clojure "1.8.0"]
-     [com.taoensso/timbre "4.8.0"]
+     [com.taoensso/timbre "4.10.0"]
+     [com.fzakaria/slf4j-timbre "0.3.5"]
      [org.clojure/core.incubator "0.1.4"]
      [com.fzakaria/slf4j-timbre "0.2"]
      [org.clojure/tools.reader "1.0.0-beta4"]]
